defdc456d140e2115a971628f9e40f7f1e15b60c
brentru <robots199@me.com>
2018-07-18 15:30:32 -0400

    update example to reflect ADC API

--- examples/mcp3xxx_simpletest.py
+++ examples/mcp3xxx_simpletest.py
@@ -1,27 +1,10 @@
-import adafruit_mcp3xxx
-from busio import SPI
 import digitalio
-from digitalio import DigitalInOut
 import board
+import busio
+from adafruit_mcp3xxx.single_ended import MCP3008
 
-spi = SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 cs = digitalio.DigitalInOut(board.D5)
-
-# create the mcp3008 object
-mcp = adafruit_mcp3xxx.MCP3008(spi, cs)
-
-"""
-Single-Ended Read
-"""
-# channel 0 raw value
-print('CH0 Value: ', mcp.read_adc(0))
-# channel 0 voltage value 
-print('CH0 Voltage:', mcp.read_volts(0), 'volts')
-
-"""
-Differential Read
-"""
-# differential 0 raw value
-print('differential 0 (CH0-CH1) value:', mcp.read_adc_difference(0))
-# differential 0 differential voltage
-print('differential 0 (CH0-CH1) value:', mcp.read_volts_difference(0))
+adc = MCP3008(spi, cs)
+print(adc[0].value)
