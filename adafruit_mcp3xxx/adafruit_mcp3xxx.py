54785708cc9ea768c12cfcc02c6d1be7e7ef8d15
brentru <robots199@me.com>
2018-07-20 11:39:45 -0400

    pylinting and cleaning up for travis

--- adafruit_mcp3xxx/adafruit_mcp3xxx.py
+++ adafruit_mcp3xxx/adafruit_mcp3xxx.py
@@ -47,85 +47,91 @@ __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MCP3xxx.git"
 
 # imports
 from micropython import const
-import digitalio
 from adafruit_bus_device.spi_device import SPIDevice
 
 # configuration values
-MCP3008_OUT_BUFF      = const(0x00)
-MCP3008_DIFF_READ     = const(0b10)
-MCP3008_SINGLE_READ   = const(0b11)
+MCP3008_OUT_BUFF = const(0x00)
+MCP3008_DIFF_READ = const(0b10)
+MCP3008_SINGLE_READ = const(0b11)
 
 # differential pin pairs -> channel
-MCP3008_DIFF_PINS = { 
-                  (0,1) : 0,
-                  (1,0) : 1,
-                  (2,3) : 2,
-                  (3,2) : 3,
-                  (4,5) : 4,
-                  (5,4) : 5,
-                  (6,7) : 6,
-                  (6,6) : 7
+MCP3008_DIFF_PINS = {
+    (0, 1) : 0,
+    (1, 0) : 1,
+    (2, 3) : 2,
+    (3, 2) : 3,
+    (4, 5) : 4,
+    (5, 4) : 5,
+    (6, 7) : 6,
+    (6, 6) : 7
 }
 
 class MCP3xxx(object):
-  def __init__(self, spi_bus, cs, max_voltage=3.3):
-    """
-    Base MCP3xxx Interface
+    """Base MCP3xxx Interface."""
 
-    :param ~busdevice.SPIDevice spi_bus: SPI bus the ADC is on.
-    :param ~digitalio.DigitalInOut cs: Chip Select pin.
-    :param int max_voltage: Maximum voltage into the ADC, defaults to 3.3v.
-    """
-    self.spi_device = SPIDevice(spi_bus, cs)
-    self.out_buf = bytearray(3)
-    self.in_buf = bytearray(3)
-    self.max_voltage = max_voltage
+    def __init__(self, spi_bus, cs, max_voltage=3.3):
+        """
+        MCP3xxx SPI Device
 
-  def _read_pin(self, pin):
-      """Subclasses should override this function to return a value for the
-      requested pin as a signed integer value.
-      """
-      raise NotImplementedError('Subclass must implement _read_pin function!')
+        :param ~busdevice.SPIDevice spi_bus: SPI bus the ADC is on.
+        :param ~digitalio.DigitalInOut cs: Chip Select pin.
+        :param int max_voltage: Maximum voltage into the ADC, defaults to 3.3v.
+        """
+        self.spi_device = SPIDevice(spi_bus, cs)
+        self.out_buf = bytearray(3)
+        self.in_buf = bytearray(3)
+        self.max_voltage = max_voltage
 
-  def _read_pin_volts(self, pin):
-      """Subclasses should override this function to return a value for the
-      requested pin as a float value.
-      """
-      raise NotImplementedError('Subclass must implement _read_pin_volts function!')
+    def reference_voltage(self):
+        """Returns the MCP3xxx's reference voltage."""
+        return self.max_voltage
 
-  def _read(self, pin, is_differential=False):
-    """SPI transfer for ADC reads.
+    def _read_pin(self, pin):
+        """Subclasses should override this function to return a value for the
+        requested pin as a signed integer value.
+        """
+        raise NotImplementedError('Subclass must implement _read_pin function!')
 
-    params:
-      int pin: individual pin or differential.
-      bool is_differential: single-ended or differential read.
-    """
-    if (pin < 0 or pin > 7):
-      raise ValueError('Pin must be a value between 0 and 7')
-    # build adc read command
-    if is_differential == True:
-      command = MCP3008_DIFF_READ << 6
-    else:
-      command = MCP3008_SINGLE_READ << 6
-    command |= ((pin & 0x07) << 3)
-    self.out_buf[0] = command
-    self.out_buf[1] = MCP3008_OUT_BUFF
-    self.out_buf[2] = MCP3008_OUT_BUFF
-    # spi transfer
-    with self.spi_device as spi:
-      spi.write_readinto(self.out_buf, self.in_buf, out_start=0, 
-        out_end=len(self.out_buf), in_start=0, in_end=len(self.in_buf))
-    # parse and ret. result (10b) 
-    result = (self.in_buf[0] & 0x01) << 9
-    result |= (self.in_buf[1] & 0xFF) << 1
-    result |= (self.in_buf[2] & 0x80) >> 7
-    result &= 0x3FF
-    return result
+    def _read_pin_volts(self, pin, voltage):
+        """Subclasses should override this function to return a value for the
+        requested pin as a float value.
+        """
+        raise NotImplementedError('Subclass must implement _read_pin_volts function!')
+
+    def _read(self, pin, is_differential=False):
+        """SPI transfer for ADC reads.
+
+        params:
+          int pin: individual pin or differential.
+          bool is_differential: single-ended or differential read.
+        """
+        if (pin < 0 or pin > 7):
+            raise ValueError('Pin must be a value between 0 and 7')
+        # build adc read command
+        if is_differential:
+            command = MCP3008_DIFF_READ << 6
+        else:
+            command = MCP3008_SINGLE_READ << 6
+        command |= ((pin & 0x07) << 3)
+        self.out_buf[0] = command
+        self.out_buf[1] = MCP3008_OUT_BUFF
+        self.out_buf[2] = MCP3008_OUT_BUFF
+        # spi transfer
+        with self.spi_device as spi:
+            spi.write_readinto(self.out_buf, self.in_buf, out_start=0,
+                               out_end=len(self.out_buf), in_start=0, in_end=len(self.in_buf))
+        # parse and ret. result (10b)
+        result = (self.in_buf[0] & 0x01) << 9
+        result |= (self.in_buf[1] & 0xFF) << 1
+        result |= (self.in_buf[2] & 0x80) >> 7
+        result &= 0x3FF
+        return result
+
 
 
 class MCP3008(MCP3xxx):
     """MCP3008 10-bit analog to digital converter instance.
-    
+
     mcp = adafruit_mcp3xxx.MCP3008(spi,cs)
     """
     def __init__(self, spi_bus, cs):
@@ -133,77 +139,78 @@ class MCP3008(MCP3xxx):
         self.pin_count = 7 #mcp3008 has 8channels.
 
     def _read_pin(self, pin):
-      """Reads a MCP3008 pin, returns the value as an integer."""
-      return self._read(pin)
+        """Reads a MCP3008 pin, returns the value as an integer."""
+        return self._read(pin)
 
     def _read_pin_volts(self, pin, voltage):
-      """Reads a MCP3008 pin, returns the voltage as a floating point value."""
-      v_in = self._read(pin)
-      return (v_in * voltage) / 1023
-    
+        """Reads a MCP3008 pin, returns the voltage as a floating point value."""
+        v_in = self._read(pin)
+        return (v_in * voltage) / 1023
+
     def _read_pin_differential(self, diff_pin):
-      """Reads a MCP3008 differential pin value, returns the value as an integer."""      
-      return self._read(diff_pin, is_differential=True)
-    
+        """Reads a MCP3008 differential pin value, returns the value as an integer."""
+        return self._read(diff_pin, is_differential=True)
+
     def _read_pin_volts_differential(self, diff_pin, voltage):
-      """Reads a MCP3008 differential pin value, returns the voltage as a floating point value."""
-      v_in = self._read(diff_pin, is_differential=True)
-      return (v_in * voltage) / 1023
+        """Reads a MCP3008 differential pin value, returns the voltage as a floating point value."""
+        v_in = self._read(diff_pin, is_differential=True)
+        return (v_in * voltage) / 1023
 
 
 class AnalogIn(object):
-  """AnalogIn for single-ended ADC readings.
-  
-  adc = adafruit_mcp3xxx.AnalogIn(mcp, pin)
+    """AnalogIn for single-ended ADC readings.
 
-  :param adc: mcp3xxx object.
-  :param pin: mcp3xxx analog pin.
-  """
-  def __init__(self, adc, pin):
-    self._adc = adc
-    self._pin = pin
+    adc = adafruit_mcp3xxx.AnalogIn(mcp, pin)
 
-  @property
-  def value(self):
-    """Returns the value of an ADC pin as an integer.
+    :param adc: mcp3xxx object.
+    :param pin: mcp3xxx analog pin.
     """
-    return self._adc._read_pin(self._pin)
+    def __init__(self, adc, pin):
+        self._adc = adc
+        self._pin = pin
 
-  @property
-  def volts(self):
-    """Returns the voltage from an ADC pin as a floating point value.
-    """
-    return self._adc._read_pin_volts(self._pin, self._adc.max_voltage)
+    @property
+    def value(self):
+        """Returns the value of an ADC pin as an integer.
+        """
+        return self._adc._read_pin(self._pin)
+
+    @property
+    def volts(self):
+        """Returns the voltage from an ADC pin as a floating point value.
+        """
+        return self._adc._read_pin_volts(self._pin, self._adc.max_voltage)
 
 
 class AnalogIn_Differential(object):
-  """Reads the difference between two signals
+    """Reads the difference between two signals
 
-  adc = adafruit_mcp3xxx.AnalogIn_Differential(mcp, pin_1, pin_2)
+    adc = adafruit_mcp3xxx.AnalogIn_Differential(mcp, pin_1, pin_2)
 
-  :param adc: mcp3xxx object.
-  :param pin_1: mcp3xxx analog pin 1.
-  :param pin_2: mcp3xxx analog pin 2.
-  """
-
-  def __getitem__(self, key):
-    return self._channels[MCP3008_DIFF_PINS[key]]
-
-  def __init__(self, adc, pin_1, pin_2):
-    self._adc = adc
-    self._pin_1 = pin_1
-    self._pin_2 = pin_2
-
-  @property
-  def value(self):
-    """Returns the value from a differential read across two pins as an integer.
+    :param adc: mcp3xxx object.
+    :param pin_1: mcp3xxx analog pin 1.
+    :param pin_2: mcp3xxx analog pin 2.
     """
-    diff_pin = MCP3008_DIFF_PINS.get((self._pin_1,self._pin_2), "Difference pin not found.")
-    return self._adc._read_pin_differential(diff_pin)
 
-  @property
-  def volts(self):
-    """Returns the voltage from a differential read across two pins as a floating point value.
-    """
-    diff_pin = MCP3008_DIFF_PINS.get((self._pin_1,self._pin_2), "Difference pin not found.")
-    return self._adc._read_pin_volts_differential(diff_pin, self._adc.max_voltage)
+    def __getitem__(self, key):
+        return self._channels[MCP3008_DIFF_PINS[key]]
+
+    def __init__(self, adc, pin_1, pin_2):
+        self._adc = adc
+        self._channels = []
+        self._pin_1 = pin_1
+        self._pin_2 = pin_2
+
+    @property
+    def value(self):
+        """Returns the value from a differential read across two pins as an integer.
+        """
+        diff_pin = MCP3008_DIFF_PINS.get((self._pin_1, self._pin_2), "Difference pin not found.")
+        return self._adc._read_pin_differential(diff_pin)
+
+    @property
+    def volts(self):
+        """Returns the voltage from a differential read across two pins as a floating point value.
+        """
+        diff_pin = MCP3008_DIFF_PINS.get((self._pin_1, self._pin_2), "Difference pin not found.")
+        return self._adc._read_pin_volts_differential(diff_pin, self._adc.max_voltage)
